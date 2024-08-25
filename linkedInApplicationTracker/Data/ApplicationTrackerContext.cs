f503f5e7d571a98d7b4f6633fd781b32edf4c63a
Oscar Moore <oscarjmoore38@gmail.com>
2024-08-25 01:05:23 +0000

    built out context file

--- linkedInApplicationTracker/Data/ApplicationTrackerContext.cs
+++ linkedInApplicationTracker/Data/ApplicationTrackerContext.cs
@@ -1,9 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using linkedInApplicationTracker.Models;
 
 namespace linkedInApplicationTracker.Data
 {
     public class ApplicationTrackerContext : DbContext
     {
-        // Insert DB access code
+        public ApplicationTrackerContext (DbContextOptions<ApplicationTrackerContext> options)
+            :base(options)
+            {
+            }
+        public DbSet<Application> Applications {get; set;}
+        public DbSet<User> Users {get; set;}
+
+        // Explicitly configuring due to requirement for cascade delete
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+            .HasMany(u => u.Applications)
+            .WithOne(a => a.User)
+            .HasForeignKey(a => a.UserID)
+            .OnDelete(DeleteBehavior.Cascade); 
+        }
     }
 }
