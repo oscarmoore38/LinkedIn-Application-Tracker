1ada3db4917118ea301835d78e8947b3b49ca996
Oscar Moore <oscarjmoore38@gmail.com>
2024-10-12 00:02:00 +0000

    added filtering ability

--- linkedInApplicationTracker/Services/ApplicationTrackerService.cs
+++ linkedInApplicationTracker/Services/ApplicationTrackerService.cs
@@ -22,14 +22,36 @@ namespace linkedInApplicationTracker.Services
         }
 
 
-        public async Task<User?> GetUserByIdAsync(int id)
+        public async Task<User?> GetUserByIdAsync(int userID)
         {
             
             return await _ApplicationTrackerContext.Users
-                .Include(u => u.Applications)
-                .AsNoTracking()
-                .FirstOrDefaultAsync(u => u.UserID == id);
+                .FirstOrDefaultAsync(u => u.UserID == userID);
 
+        }
+
+        public IQueryable<Application> GetApplicationsByUserId(int userID, string searchString)
+        {
+            // Get applications for user 
+            var query = _ApplicationTrackerContext.Applications
+                                                .Where(a => a.UserID == userID);
+
+            // Apply additional filters if the search string is not null or empty
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToLower();
+                // Case insensitive search 
+                query = query.Where(a => 
+                    a.Company.ToLower().Contains(searchString) ||
+                    a.Title.ToLower().Contains(searchString) ||
+                    a.Outcome.ToLower().Contains(searchString));
+            }
+            return query;
+            }
+
+        public async Task<List<Application>> ExecuteApplicationsQueryAsync(IQueryable<Application> applicationIQ)
+        {
+            return await applicationIQ.AsNoTracking().ToListAsync();
         }
 
         public void UpdateUser(int id)
@@ -42,21 +64,38 @@ namespace linkedInApplicationTracker.Services
         
         }
 
-        public async Task AddApplicationAsync(Application Application)
+        public async Task<Application?> GetApplicationByIDAsync(int applicationID)
         {
-            _ApplicationTrackerContext.Add(Application);
+            return await _ApplicationTrackerContext.Applications
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(a => a.ApplicationID == applicationID);
+        }
+
+        // Use for read operations for improved preformance. 
+        public async Task<Application?> GetApplicationByIDNoTrackingAsync(int applicationID)
+        {
+            return await _ApplicationTrackerContext.Applications
+                .AsNoTracking()
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(a => a.ApplicationID == applicationID);
+        }
+
+        public async Task AddApplicationAsync(Application applicationToAdd)
+        {
+            _ApplicationTrackerContext.Applications.Add(applicationToAdd);
             await _ApplicationTrackerContext.SaveChangesAsync();
         }
 
 
-        public void UpdateApplication(Application job)
+        public async Task UpdateApplicationAsync(Application applicationToUpdate)
         {
-
+            await _ApplicationTrackerContext.SaveChangesAsync();
         }
 
-        public void DeleteApplication(int id)
+        public async Task DeleteApplicationAsync(Application applicationToDelete)
         {
-        
+            _ApplicationTrackerContext.Applications.Remove(applicationToDelete);
+            await _ApplicationTrackerContext.SaveChangesAsync();
         }
 
     }
