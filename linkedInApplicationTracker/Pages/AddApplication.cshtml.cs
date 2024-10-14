0893248bf3cbf5ebfcb9cb26896d75f9d7654d03
Oscar Moore <oscarjmoore38@gmail.com>
2024-10-14 17:47:15 +0000

    resolve FK constraint error with add application

--- linkedInApplicationTracker/Pages/AddApplication.cshtml.cs
+++ linkedInApplicationTracker/Pages/AddApplication.cshtml.cs
@@ -2,31 +2,72 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using linkedInApplicationTracker.Models;
 using linkedInApplicationTracker.Services;
+using Microsoft.Identity.Client;
 
 namespace linkedInApplicationTracker.Pages;
 
 public class AddApplication : PageModel
 {
+    private readonly ILogger<AddApplication> _logger;
     [BindProperty]
     public Application NewApplication {get; set;} = default!; 
     private readonly ApplicationTrackerService _service; 
-    public AddApplication(ApplicationTrackerService service)
+    public User? CurrentUser {get; set;} = default!;
+    public int UserID = 1; // Will update once user auth is implemented.
+    
+    
+    public AddApplication(ApplicationTrackerService service, ILogger<AddApplication> logger)
     {
+        _logger = logger;
         _service = service;
     }
-    public void OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
+        // Check current user - will update and change code once auth is implemented
+        if (UserID == null)
+        {
+            return NotFound();
+        }
+        
+        try 
+        {
+            CurrentUser = await _service.GetUserByIdAsync(UserID);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while finding user with ID: {UserID}", UserID);
+
+            ModelState.AddModelError(string.Empty, "An error occurred while finding user.");
+        }
+
+        if (CurrentUser == null)
+        {
+            return NotFound();
+        }
+
+        return Page();
     }
-    public async Task<IActionResult> OnPostAsync()
+    public async Task<IActionResult> OnPostAsync(int id)
     {
         if(!ModelState.IsValid || NewApplication == null)
         {
             return Page();
         }
 
-        await _service.AddApplicationAsync(NewApplication);
+        Console.WriteLine($"Here is the current ID: {id}");
 
-        return RedirectToAction("Get");
+        try
+        {
+            await _service.AddApplicationAsync(NewApplication);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while adding the following application: {NewApplication}", NewApplication);
+
+            ModelState.AddModelError(string.Empty, "An error occurred while adding application.");
+
+        }
+        return RedirectToPage("./ManageApplication");
         
     }
 }
