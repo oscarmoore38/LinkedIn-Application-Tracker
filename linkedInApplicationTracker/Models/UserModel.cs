7b09fde785b8526dd0e759db58768cf7c23334d0
Oscar Moore <oscarjmoore38@gmail.com>
2024-11-07 13:54:17 +0000

    Added navigation property to User model for identity UserAuth

--- linkedInApplicationTracker/Models/UserModel.cs
+++ linkedInApplicationTracker/Models/UserModel.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography.X509Certificates;
+using linkedInApplicationTracker.Areas.Identity.Data;
 
 
 namespace linkedInApplicationTracker.Models
@@ -12,6 +13,7 @@ namespace linkedInApplicationTracker.Models
         public string FirstName {get; set;} = default!;
         public string LastName {get; set;} = default!;
         public ICollection<Application> Applications {get;set;} = default!;
+        public AuthUser AuthUser { get; set; } = default!; // Navigation property
 
     }
 }
